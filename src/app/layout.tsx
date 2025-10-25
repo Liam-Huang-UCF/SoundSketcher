8a086fbcbb554f5f90492eebb03f4ede587bcc1f
Liam Huang <li104704@ucf.edu>
2025-10-25 01:15:48 -0400

    made ui/ux changes such as changing background color, changing ui/ux formatting to be universally handled by globals.css and not within each page.tsx, and added floating notes in background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { type Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import MusicBackground from "./_components/MusicBackground";
 
 export const metadata: Metadata = {
   title: "Create T3 App",
@@ -22,7 +23,10 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable}`}>
       <body>
-        <TRPCReactProvider>{children}</TRPCReactProvider>
+        <MusicBackground />
+        <TRPCReactProvider>
+          <div className="app-content">{children}</div>
+        </TRPCReactProvider>
       </body>
     </html>
   );
